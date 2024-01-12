6de775e8a272284271f7cce891548bf53e6dcc8b
Shamyuktha.V <vshamyuktha@gmail.com>
2024-01-12 12:15:55 +0530

    milestone1 completed

--- 21pt34_solution/milestone1.py
+++ 21pt34_solution/milestone1.py
@@ -5,7 +5,7 @@ import csv
 
 x1,y1,x2,y2=sp.symbols(('x1','y1','x2','y2'))
 
-file=open("Workshop2024\\Milestone1\\Input\\Testcase1.txt","r")
+file=open("Workshop2024\\Milestone1\\Input\\Testcase4.txt","r")
 details_wafer=dict()
 
 lines=file.read().splitlines()
@@ -14,8 +14,7 @@ for words in lines:
     details_wafer[word[0]]=int(word[1])
 
 interval=details_wafer["WaferDiameter"]/(details_wafer["NumberOfPoints"]-1)
-slope_1=np.tan(details_wafer["Angle"])
-print(slope_1)
+slope_1=np.tan(details_wafer["Angle"]*(np.pi/180))
 
 start_x=0
 start_y=0
@@ -35,11 +34,8 @@ for i in range(details_wafer["NumberOfPoints"]-2):
     eq2=sp.Eq((x1-diameter_x1)**2+(y1-diameter_y1)**2-(interval)**2,0)
     eq2=sp.Eq((x1-diameter_x2)**2+(y1-diameter_y2)**2-(details_wafer["WaferDiameter"]-((i+1)*interval))**2,0)
     list_points.append(sp.solve((eq1,eq2),(x1,y1))[0])
-
-print(coordinates)
 list_points.append(coordinates[1])
-print(list_points)
-with open('21pt34_solution\\milestone1testcase1.txt','w',newline='') as file1:
+with open('21pt34_solution\\milestone1testcase4.txt','w',newline='') as file1:
     writer=csv.writer(file1)
     for points in list_points:
         writer.writerow(points)
